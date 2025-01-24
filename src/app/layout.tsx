1d6e1aa790073c2b9635d205dbbc7e693b2ebadf
suraj <suraj@funnelstory.io>
2025-01-24 19:38:11 +0530

    add hotjar tag integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import { ThemeProvider } from "@/components/theme-provider";
+import Script from "next/script";
 import { icons } from "lucide-react";
 
 const outfit = Outfit({ subsets: ["latin"] });
@@ -22,6 +23,23 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
+        <Script
+          id="hotjaranalytics"
+          src="/theme.js"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+          (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:1839735,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `,
+          }}
+        />
       </head>
       <body className={outfit.className}>
         <ThemeProvider attribute="class" defaultTheme="light">
