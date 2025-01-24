e12f2c34ceabcfd48bfbfe3cc613b83be4c8ded2
suraj <suraj@funnelstory.io>
2025-01-24 19:21:09 +0530

    enhance video player controls

--- src/components/video-player.tsx
+++ src/components/video-player.tsx
@@ -15,6 +15,7 @@ export function VideoPlayer({ videoUrl, thumbnailUrl }: VideoPlayerProps) {
   const videoRef = useRef<HTMLVideoElement>(null);
 
   const [completed, setCompleted] = useState(0);
+  const [buffered, setBuffered] = useState(0);
 
   useEffect(() => {
     let videoPlayerRef = videoRef.current;
@@ -22,19 +23,35 @@ export function VideoPlayer({ videoUrl, thumbnailUrl }: VideoPlayerProps) {
       videoPlayerRef.addEventListener("timeupdate", (e) => {
         const completed =
           (videoPlayerRef?.currentTime / videoPlayerRef?.duration) * 100;
+        if (Math.round(completed) >= 100.0) {
+          setIsPlaying(false);
+        }
         setCompleted(completed);
+      });
+
+      videoPlayerRef.addEventListener("progress", (e) => {
+        const buffered =
+          (videoPlayerRef?.buffered.end(videoPlayerRef?.buffered.length - 1) /
+            videoPlayerRef?.duration) *
+          100;
+        setBuffered(buffered);
       });
     }
     return () => {
       videoPlayerRef?.removeEventListener("timeupdate", () => {});
+      videoPlayerRef?.removeEventListener("progress", () => {});
     };
   }, []);
 
-  if (isPlaying) {
-    videoRef.current?.play();
-  } else {
-    videoRef.current?.pause();
-  }
+  const handleVideoClick = (play: boolean) => {
+    if (play) {
+      videoRef.current?.play();
+      setIsPlaying(play);
+    } else {
+      videoRef.current?.pause();
+      setIsPlaying(play);
+    }
+  };
 
   return (
     // <IKVideo
@@ -54,16 +71,25 @@ export function VideoPlayer({ videoUrl, thumbnailUrl }: VideoPlayerProps) {
         muted
         className="w-full h-full object-cover"
       />
+
       {/** progress bar */}
       <div className="absolute bottom-0 left-0 w-full h-1.5 bg-gray-200">
-        <div
-          style={{ width: `${completed}%` }}
-          className="h-full bg-primary"
-        ></div>
+        <div className="h-full relative">
+          <div
+            style={{ width: `${completed}%` }}
+            className="h-full absolute bg-primary"
+          ></div>
+          {/** buffer bar */}
+          <div
+            style={{ width: `${buffered}%` }}
+            className="h-full absolute bg-primary/50"
+          ></div>
+        </div>
       </div>
+
       {/** play icon */}
       <span
-        onClick={() => setIsPlaying(!isPlaying)}
+        onClick={() => handleVideoClick(!isPlaying)}
         className={`absolute top-1/2 left-1/2 cursor-pointer transform -translate-x-1/2 -translate-y-1/2 ${
           isPlaying ? "hidden" : ""
         }`}
@@ -73,7 +99,7 @@ export function VideoPlayer({ videoUrl, thumbnailUrl }: VideoPlayerProps) {
 
       {/** Pause icon */}
       <span
-        onClick={() => setIsPlaying(!isPlaying)}
+        onClick={() => handleVideoClick(!isPlaying)}
         className={`absolute right-0 bottom-0 cursor-pointer transform -translate-x-1/2 -translate-y-1/2 ${
           isPlaying ? "" : "hidden"
         }`}
